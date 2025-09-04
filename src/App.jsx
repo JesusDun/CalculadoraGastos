dcd63b46b0930a5f7bbd12dd0ac6e6e255d17bdb
JesusDun <alejandrovielmagarcia@gmail.com>
2025-09-04 11:21:25 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseCalculator from './components/ExpenseCalculator';
+import './styles/ExpenseCalculator.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseCalculator />
+    </div>
+  );
+}
+
+export default App;
